d9c89c7efce8f8acddce5557397ef5134586b0c8
sanjayramsundar <64749365+sanjayramsundar@users.noreply.github.com>
2020-08-22 12:03:12 -0700

    Add files via upload

---
+++ clock/clockcode.js
@@ -0,0 +1,14 @@
+function currentTime() {
+    var date = new Date();
+    var hour = date.getHours();   
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    hour = updateTime(hour);
+    minutes = updateTime (minutes);
+    seconds = updateTime(seconds);
+}
+
+function updateTime(){
+    
+}
+
